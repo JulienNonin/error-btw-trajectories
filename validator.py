ed135b8310e0f6d42d70d7c6260b85462d65b7a6
Maun <julien.nonin@sfr.fr>
2019-09-16 16:42:50 +0200

    (Feat) Create a function to compute the intersection of two line segments

--- validator.py
+++ validator.py
@@ -6,7 +6,7 @@ import numpy as np
 from os import listdir
 from matplotlib.ticker import MaxNLocator
 
-def test(estimator, epsilon = .00001, samples_range = slice(0, None), display = True):
+def test(estimator, epsilon = .00001, samples_range = slice(0, None), display = False):
     """Tests whether the function 'estimator' correctly estimates the
     difference between two trajectories. A battery of tests contained
     in the test folder is used for this purpose. It raises an `Assertion-
@@ -22,11 +22,12 @@ def test(estimator, epsilon = .00001, samples_range = slice(0, None), display = 
             * slice(0,4): select the first 4 tests
             * slice(3, None): select from the 3rd
         - display: boolean - if True plots the trajectories
-    Returns : True if all tests have been validated
+    Returns : True if all tests have been validated, False if any has failed
     """
     dirname = "test/" # directory where the test files are located
+    validated = True # boolean returned by the function
     for filename in sorted(listdir(dirname)[samples_range]):
-        if "[test" in filename: # for each test
+        if "test" in filename: # for each test
             print(filename, end="\t")
             
             # Fetching data
@@ -40,17 +41,21 @@ def test(estimator, epsilon = .00001, samples_range = slice(0, None), display = 
             
             # Plotting trajectories
             if display:
-                plt.clf()
+                plt.figure()
+            #    plt.clf()
                 plt.plot(*X.transpose(), '--o', *Y.transpose(), '--o')
                 plt.axis("equal")
                 plt.grid()
+                plt.title(filename)
             
             # Comparing the expected value with the estimation computes by
             # the estimator, with a tolerance of +/- epsilon
-            assert res_true - epsilon <= res <= res_true + epsilon, \
-                f"The expected value for {filename} is {res_true}, not {res}."
-            print("ok")
-            return True
+            if res_true - epsilon <= res <= res_true + epsilon:
+                print(f"ok \t The expected value is indeed {res}")
+            else :
+                print(f"NOT ok \t The expected value is {res_true}, but the output value is {res}.")
+                validated = False
+    return validated
 
 if __name__ == "__main__":
     estimate = lambda X, Y : np.abs(np.trapz(*X.transpose()[::-1]) - np.trapz(*Y.transpose()[::-1]))
