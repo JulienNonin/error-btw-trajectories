7c0f021c07cd799ca574ee56cb19f23eb0b3fbbe
Maun <julien.nonin@sfr.fr>
2019-10-14 20:46:03 +0200

    (Clean) remove utils.py

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -3,8 +3,35 @@ import numpy as np
 import os
 from tests.utils import fetch_data
 from unittest.mock import patch # prevent ploting figures
+import trajectories_error.estimator as estimator
 
 current_dir = os.path.dirname(__file__)
+
+
+def fetch_data(filename, contains_solution=True, criterion='.txt', sep=','):
+    """
+    >>> fetch_data("tests/shared-oracles/Oracles/CorrectInputTrajectories/10_parallelTrajectories.txt")[1:]
+    [Trajectory([Point(1.0, 1.0), Point(3.0, 1.0)]), Trajectory([Point(1.0, 2.0), Point(3.0, 2.0)]), 1.0, 0.001]
+    >>> fetch_data("tests/shared-oracles/Oracles/IncorrectInputTrajectories/3_EmptyAcquiredTrajectory.txt", False)[1:]
+    [Trajectory([Point(0.0, 0.0), Point(1.0, 0.0)]), Trajectory([])]
+    >>> fetch_data("tests/shared-oracles/Oracles/IncorrectInputTrajectories/2_EmptyAcquiredTrajectory.txt", False)[1:]
+    [Trajectory([Point(0.0, 1.0)]), Trajectory([])]
+    """
+    with open(filename, "r") as file:
+        lines = file.read().splitlines() # getting rid of \n
+        data = [[float(n) for n in line.split(sep) if n] for line in lines] # parse data
+        
+    reference_coord = list(zip(data[0], data[1])) # line 0 : x-axis of the reference trajectory, line 1 : y-axis
+    acquired_coord = list(zip(data[2], data[3]))  # line 2 : x-axis of the acquired trajectory, line 3 : y-axis
+    reference = estimator.Trajectory([estimator.Point(x, y) for x, y in reference_coord])
+    acquired = estimator.Trajectory([estimator.Point(x, y) for x, y in acquired_coord])
+
+    if contains_solution:
+        expected_output, = data[4] or [-1]
+        epsilon, = data[5] or [-1]
+        return [filename, reference, acquired, expected_output, epsilon]
+    else:
+        return [filename, reference, acquired]
 
 def fetch_test_data(dirname, contains_solution=True, criterion='.txt', sep=',', samples=slice(0, None)):
     """
--- trajectories_error/estimator.py
+++ trajectories_error/estimator.py
@@ -12,7 +12,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
-import tests.utils as utils
 
 
 #%%
@@ -283,11 +282,37 @@ class Trajectory():
         return error / self.length
 
 
+def fetch_data(filename, contains_solution=True, criterion='.txt', sep=','):
+    """
+    >>> fetch_data("tests/shared-oracles/Oracles/CorrectInputTrajectories/10_parallelTrajectories.txt")[1:]
+    [Trajectory([Point(1.0, 1.0), Point(3.0, 1.0)]), Trajectory([Point(1.0, 2.0), Point(3.0, 2.0)]), 1.0, 0.001]
+    >>> fetch_data("tests/shared-oracles/Oracles/IncorrectInputTrajectories/3_EmptyAcquiredTrajectory.txt", False)[1:]
+    [Trajectory([Point(0.0, 0.0), Point(1.0, 0.0)]), Trajectory([])]
+    >>> fetch_data("tests/shared-oracles/Oracles/IncorrectInputTrajectories/2_EmptyAcquiredTrajectory.txt", False)[1:]
+    [Trajectory([Point(0.0, 1.0)]), Trajectory([])]
+    """
+    with open(filename, "r") as file:
+        lines = file.read().splitlines() # getting rid of \n
+        data = [[float(n) for n in line.split(sep) if n] for line in lines] # parse data
+        
+    reference_coord = list(zip(data[0], data[1])) # line 0 : x-axis of the reference trajectory, line 1 : y-axis
+    acquired_coord = list(zip(data[2], data[3]))  # line 2 : x-axis of the acquired trajectory, line 3 : y-axis
+    reference = Trajectory([Point(x, y) for x, y in reference_coord])
+    acquired = Trajectory([Point(x, y) for x, y in acquired_coord])
+
+    if contains_solution:
+        expected_output, = data[4] or [-1]
+        epsilon, = data[5] or [-1]
+        return [filename, reference, acquired, expected_output, epsilon]
+    else:
+        return [filename, reference, acquired]
+
+
 #%%
 if __name__ == "__main__":
     dirname = "tests/shared-oracles/Oracles/EvaluationTrajectories"
     filename = "/oracle_etienne.txt"
-    filename, A, B = utils.fetch_data(dirname + filename, contains_solution=False)
+    filename, A, B = fetch_data(dirname + filename, contains_solution=False)
     A, B = Trajectory(A.points), Trajectory(B.points) # inside notebook to use the Trajectory class defined in notebook instead of using the one given by the estimator.py
     print(A.error_with(B, display=True))
     plt.title(filename.split("/")[-1])
