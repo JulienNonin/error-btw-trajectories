ce73d3b139ff89c9f3784b5677e8301b50b0f488
Maun <34622694+JulienNonin@users.noreply.github.com>
2019-10-18 18:44:24 +0200

    (Fix) Update cache of circleci (#11)
    * (Fix) fetch_data

--- tests/test_estimator.py
+++ tests/test_estimator.py
@@ -7,47 +7,11 @@ import os
 from unittest.mock import patch  # prevent ploting figures
 import trajectories_error.estimator as estimator
 
-
-def fetch_data(filename, contains_solution=True, sep=','):
-    """
-    >>> fetch_data("tests/shared-oracles/Oracles/CorrectInputTrajectories\
-/10_parallelTrajectories.txt")[1:]
-    [Trajectory([Point(1.0, 1.0), Point(3.0, 1.0)]),\
- Trajectory([Point(1.0, 2.0), Point(3.0, 2.0)]), 1.0, 0.001]
-    >>> fetch_data("tests/shared-oracles/Oracles/IncorrectInputTrajectories\
-/3_EmptyAcquiredTrajectory.txt", False)[1:]
-    [Trajectory([Point(0.0, 0.0), Point(1.0, 0.0)]),\
- Trajectory([])]
-    >>> fetch_data("tests/shared-oracles/Oracles/IncorrectInputTrajectories\
-/2_EmptyAcquiredTrajectory.txt", False)[1:]
-    [Trajectory([Point(0.0, 1.0)]), Trajectory([])]
-    """
-    with open(filename, "r") as file:
-        lines = file.read().splitlines()  # getting rid of \n
-        data = [[float(n) for n in line.split(sep) if n]
-                for line in lines]  # parse data
-
-    # line 0 : x-axis of the reference trajectory, line 1 : y-axis
-    reference_coord = list(zip(data[0], data[1]))
-    # line 2 : x-axis of the acquired trajectory, line 3 : y-axis
-    acquired_coord = list(zip(data[2], data[3]))
-    reference = estimator.Trajectory(
-        [estimator.Point(x, y) for x, y in reference_coord])
-    acquired = estimator.Trajectory(
-        [estimator.Point(x, y) for x, y in acquired_coord])
-
-    if contains_solution:
-        expected_output, = data[4] or [-1]
-        epsilon, = data[5] or [-1]
-        return [filename, reference, acquired, expected_output, epsilon]
-    return [filename, reference, acquired]
-
-
 def fetch_test_data(dirname, contains_solution=True, criterion='.txt',
                     sep=',', samples=slice(0, None)):
     """
     >>> fetch_test_data("shared-oracles/Oracles/CorrectInputTrajectories/")[0][1:]
-    [Trajectory([Point(1.0, 1.0), Point(3.0, 1.0)]),\
+    [Trajectory([Point(1.0, 1.0), Point(3.0, 1.0)]), \
 Trajectory([Point(1.0, 2.0), Point(3.0, 2.0)]), 1.0, 0.001]
     >>> fetch_test_data("shared-oracles/Oracles/IncorrectInputTrajectories/", False)[2][1:]
     [Trajectory([Point(0.0, 0.0), Point(1.0, 0.0)]), Trajectory([])]
@@ -60,7 +24,7 @@ Trajectory([Point(1.0, 2.0), Point(3.0, 2.0)]), 1.0, 0.001]
         dirname) if criterion in filename]
     tests_sample = []
     for filename in sorted(test_file_names)[samples]:
-        tests_sample.append(fetch_data(
+        tests_sample.append(estimator.fetch_data(
             dirname + filename, contains_solution=contains_solution, sep=sep))
     return tests_sample
 
